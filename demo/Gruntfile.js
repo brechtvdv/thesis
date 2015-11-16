600a5014d5575ba7d2308866ca41aa7f93cc10ee
brechtvdv <br5cht@hotmail.com>
2015-11-16 13:15:14 +0100

    Setup demo with Grunt, Bootstrap, Leaflet

---
+++ demo/Gruntfile.js
@@ -0,0 +1,88 @@
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
+module.exports = function(grunt) {
+
+  // ===========================================================================
+  // CONFIGURE GRUNT ===========================================================
+  // ===========================================================================
+  grunt.initConfig({
+
+    // get the configuration info from package.json ----------------------------
+    // this way we can use things like name and version (pkg.name)
+    pkg: grunt.file.readJSON('package.json'),
+
+    // all of our configuration will go here
+    // configure jshint to validate js files -----------------------------------
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
+      },
+
+      // when this task is run, lint the Gruntfile and all js files in src
+      build: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    // configure uglify to minify js files -------------------------------------
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/js/main.min.js': 'src/**/*.js'
+        }
+      }
+    },
+
+    // configure cssmin to minify css files ------------------------------------
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          'dist/css/style.min.css': 'src/css/style.css'
+        }
+      }
+    },
+
+    // compile sass stylesheets to css -----------------------------------------
+    sass: {
+      dist: {
+        files: {
+          'dist/css/style.css' : 'src/css/style.scss'
+        }
+      }
+    },
+
+    // configure watch to auto update ----------------
+    watch: {
+      
+      // for stylesheets, watch css and sass files 
+      // only run sass and cssmin stylesheets: { 
+      files: ['src/**/*.css', 'src/**/*.scss'], 
+      tasks: ['sass', 'cssmin'],
+
+      // for scripts, run jshint and uglify 
+      scripts: { 
+        files: 'src/**/*.js', tasks: ['jshint', 'uglify'] 
+      } 
+    }
+  });
+
+  // ============= // CREATE TASKS ========== //
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'sass']);
+
+  // ===========================================================================
+  // LOAD GRUNT PLUGINS ========================================================
+  // ===========================================================================
+  // we can only load these if they are in our package.json
+  // make sure you have run npm install so our app can find these
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
+
