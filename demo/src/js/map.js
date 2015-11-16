5c5dbd64a2f8bf24777f896f0cc16c84c0198a29
brechtvdv <br5cht@hotmail.com>
2015-11-16 17:48:18 +0100

    Load stops clientside in Gazel

--- demo/src/js/map.js
+++ demo/src/js/map.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([50.893, 5.702], 7);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
